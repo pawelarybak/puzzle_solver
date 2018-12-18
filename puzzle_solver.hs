02ece48007794df13ddedff4e2d35c4c57412c3b
someoddperson <pawel.a.rybak@gmail.com>
2018-12-18 08:56:12 +0100

    Transforming file input to rows, cols and diagonals

--- puzzle_solver.hs
+++ puzzle_solver.hs
@@ -1,7 +1,35 @@
 
 
 
-puzzleIn path = do cont <- readFile path 
-                   let values = lines cont
-                   return values
+puzzle puzzlePath wordListPath = do
+    puzzleContent <- (readFile puzzlePath)
+    let strings = transformPuzzle puzzleContent
+    wordListConent <- readFile wordListPath
+    let wordList = lines wordListConent
+    return (strings, wordList)
 
+transformPuzzle :: String -> [String]
+transformPuzzle cont = lines cont 
+                        ++ transpose (lines cont) 
+                        ++ getDiagonals (lines cont)
+                        ++ getDiagonals (invert (lines cont))
+
+getDiagonals :: [[a]] -> [[a]]
+getDiagonals str = [mainDiagonal str] ++ lowerTriangle str ++ lowerTriangle (transpose str)
+
+transpose :: [[a]]-> [[a]]
+transpose ([]:_) = []
+transpose x      = (map head x) : transpose (map tail x)
+
+mainDiagonal :: [[a]] -> [a]
+mainDiagonal []      = []
+mainDiagonal ([]:xs) = []
+mainDiagonal (x:xs)  = head x : mainDiagonal (map tail xs)
+
+lowerTriangle :: [[a]] -> [[a]]
+lowerTriangle (x:y:[]) = [[head y]]
+lowerTriangle (x:xs)   = (mainDiagonal xs):(lowerTriangle xs)
+
+invert :: [a] -> [a]
+invert []     = []
+invert (x:xs) = (invert xs) ++ [x]
