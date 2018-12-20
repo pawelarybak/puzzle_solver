56aaefe96b353ba3f36edf75ef6f668743a62310
Michał Pilarek <mtpilarek@gmail.com>
2018-12-20 13:33:01 +0100

    MPilarek: -numerowanie komórek w tabeli 2D -odczytywanie rozwiązania i potrzebne funkcje pośrednie

--- puzzle_solver.hs
+++ puzzle_solver.hs
@@ -1,4 +1,39 @@
+readSolution :: [[(Char, Int)]] -> String
+readSolution a = (getKeys . sortByValue . filterUniqueKeys . concat) a
 
+getKeys:: [(a, b)] -> [a]
+getKeys ((a,b):c) = a:getKeys(c)
+getKeys [] = []
+
+sortByValue:: [(a, Int)] -> [(a, Int)]
+sortByValue [] = []
+sortByValue ((x,i1):xs) = sortByValue (filter (lessTupleValue (x,i1)) xs)
+ ++ [(x,i1)]
+ ++ sortByValue (filter (moreEqualTupleValue (x,i1)) xs)
+
+lessTupleValue::Ord b =>  (a,b) -> (a,b) -> Bool
+lessTupleValue (a,b) (c,d) = b > d
+
+moreEqualTupleValue::Ord b => (a,b) -> (a,b) -> Bool
+moreEqualTupleValue (a,b) (c,d) = b <= d
+
+
+filterUniqueKeys::Eq a => [(a, b)] -> [(a, b)]
+filterUniqueKeys ((a,c):b)
+ | (keyElem b a) = (filterUniqueKeys b)
+ | otherwise = (a,c):filterUniqueKeys(b)
+filterUniqueKeys [] = []
+
+keyElem ::Eq a=> [(a,b)]-> a -> Bool
+keyElem ((a,b):c) d = (a == d) || (keyElem c d)
+keyElem [] a = False
+
+numerateArrays ::  [[a]] -> Int ->[[(a,Int)]]
+numerateArrays ((a):b) c = (numerateRows a c):(numerateArrays b ((length a) + c))
+numerateArrays ([]) _ = []
+numerateRows ::  [a] -> Int ->[(a, Int)]
+numerateRows (a:b) c = (a,c):(numerateRows b (c+1))
+numerateRows [] _ = []
 
 
 puzzle puzzlePath wordListPath = do
